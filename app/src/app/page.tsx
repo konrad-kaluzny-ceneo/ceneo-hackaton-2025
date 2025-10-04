d4985421a22945af6471bc2f52fe33a513ca4b1c
Konrad Kałużny <konrad.kaluzny@ceneo.pl>
2025-10-04 22:39:18 +0200

    Implement user redirection based on context items in Home component; utilize UserAccessor and Repository for user ID retrieval and context management.

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,8 +1,22 @@
 import ExampleOfDynamicButton from "@/components/examples/ExampleOfDynamicButton";
 import { Friends } from "@/components/user/Friends";
 import Link from "next/link";
+import { useUserId } from "@/infrastructure/UserAccessor";
+import { Repository } from "@/infrastructure/Repository";
+import { inject } from "@/infrastructure/DIContainer";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const userId = await useUserId();
+  console.log("userId", userId);
+  const repository = inject(Repository);
+  const contextItems = repository.getContextItems(userId);
+  if (contextItems.length === 0) {
+    return redirect("/questionnaire");
+  } else {
+    return redirect("/trip-propositions");
+  }
+
   return (
     <div className="flex justify-center items-center h-screen flex-col gap-4">
       <ExampleOfDynamicButton label="Click me" />
