be33b8db4382d3afcb0e2bc2bf1f1e108e5abb29
Maciej Padula <maciej.padula@ceneo.pl>
2025-10-04 13:23:07 +0200

    fix

--- app/src/infrastructure/DIContainer.ts
+++ app/src/infrastructure/DIContainer.ts
@@ -1,12 +1,10 @@
 import { AnswerHandler } from "@/features/user-context/AnswerHandler";
 import { IRepository } from "../features/IRepository";
 import { InMemoryRepository } from "./InMemoryRepository";
-import { GetUserAnswersHandler } from "@/features/user-context/GetUserAnswersHandler";
 
 interface DIContainer {
   repository: IRepository;
   answerHadler: AnswerHandler;
-  getUserAnswersHandler: GetUserAnswersHandler;
 }
 
 const repo = new InMemoryRepository();
@@ -15,7 +13,6 @@ const repo = new InMemoryRepository();
 const container: DIContainer = {
   repository: repo,
   answerHadler: new AnswerHandler(repo),
-  getUserAnswersHandler: new GetUserAnswersHandler(repo),
 };
 
 export default container;
