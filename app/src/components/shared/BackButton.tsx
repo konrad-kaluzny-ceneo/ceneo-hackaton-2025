0aea9b8bd8e38cccf7e077d8c949dc9915a1d845
Konrad Kałużny <konrad.kaluzny@ceneo.pl>
2025-10-04 18:06:06 +0200

    fix back

--- app/src/components/shared/BackButton.tsx
+++ app/src/components/shared/BackButton.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowLeftIcon, HomeIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
 export default function BackButton() {
-    return (
-        <div>
-            <h1>BackButton</h1>
+  const pathname = usePathname();
+  const router = useRouter();
+  const isMainPage = pathname === "/";
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  return (
+    <div>
+      {isMainPage ? (
+        <div className={cn(buttonVariants({ variant: "ghost" }))}>
+          <HomeIcon className="w-8 h-8" aria-label="Strona główna" />
         </div>
-    )
+      ) : (
+        <Button onClick={handleBack} variant="ghost">
+          <ArrowLeftIcon className="w-8 h-8" aria-label="Wróć" />
+        </Button>
+      )}
+    </div>
+  );
 }
--- app/src/components/shared/Navbar.tsx
+++ app/src/components/shared/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import BackButton from "./BackButton";
 
 export default function Navbar() {
+
   return (
     <div className="flex items-center p-4 gap-4">
       <BackButton />
