cee784922ba3472beb6194207922041cbb508d5b
Bartosz Konieczny <bartosz.konieczny@ceneo.pl>
2025-10-04 20:52:22 +0200

    zmiana typów

--- app/src/types/trip-set.d.ts
+++ app/src/types/trip-set.d.ts
@@ -1,36 +1,12 @@
-export interface Location {
-  country: string;
-  region: string;
-  city: string;
-}
-
-export interface Transport {
-  from: Location;
-  fromDate: string;
-  destination: Location;
-  destinationDate: string;
-  price: number;
-  name: string;
-}
-
-export interface Accommodation {
-  location: Location;
-  date: string;
-  price: number;
-  beds: number;
-  name: string;
-  description: string;
-  images: string[];
-}
-
 export interface Destination {
   order: number;
-  transport: Transport;
-  accommodation: Accommodation | null;
+  transportId: string;
+  accommodationId: string | null;
 }
 
 export interface TripSet {
   id: string;
+  userId: string;
   name: string;
   totalPrice: number;
   duration: number;
