65a29c76ebd314f1a2e62ba93f39d0751033c1c0
Maciej Padula <maciej.padula@ceneo.pl>
2025-10-04 22:54:05 +0200

    fix

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,37 +3,27 @@
 import ExampleOfDynamicButton from "@/components/examples/ExampleOfDynamicButton";
 import { Friends } from "@/components/user/Friends";
 import Link from "next/link";
-import { useUserId } from "@/infrastructure/UserAccessor";
-import { Repository } from "@/infrastructure/Repository";
-import { inject } from "@/infrastructure/DIContainer";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useUser } from "@/infrastructure/FrontendUserAccessor";
 
 export default function Home() {
-  const [completionResult, setCompletionResult] = useState<any>(null);
-  const userId = useUser().id;
+  const router = useRouter();
 
-  useEffect(() =>{
-    const userId = useUser().id;
+  useEffect(() => {
     fetch('/api/questionnaire/completed')
       .then(response => response.json())
       .then(data => {
-        setCompletionResult(data);
+        if (data) {
+          if (!data.isCompleted) {
+            router.push("/start");
+          } else if (data.isGenerated) {
+            router.push("/trip-propositions");
+          } else {
+            router.push("/generating-trips");
+          }
+        }
       });
-  }, []);
-
-  if (completionResult) {
-    if (!completionResult.isCompleted) {
-      redirect("/start");
-    }
-
-    if (completionResult.isGenerated) {
-      redirect("/trip-propositions");
-    }
-
-    redirect("/generating-trips");
-  }
+  }, [router]);
 
   return (
     <div className="flex justify-center items-center h-screen flex-col gap-4">
