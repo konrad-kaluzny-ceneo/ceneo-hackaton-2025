3617b5bc223a2462d60db5563a2db941dd3a2f0e
Konrad Kałużny <konrad.kaluzny@ceneo.pl>
2025-10-05 00:17:25 +0200

    fix build and add skeleton

--- app/src/app/trip-propositions/page.tsx
+++ app/src/app/trip-propositions/page.tsx
@@ -3,6 +3,8 @@
 import { TripSet } from "@/types/trip-set";
 import TripPropositionsClient from "@/components/trip/TripPropositionsClient";
 import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
 
 export default function TripPropositionsPage() {
   const [trips, setTrips] = useState<TripSet[]>([]);
@@ -24,9 +26,21 @@ export default function TripPropositionsPage() {
     fetchTrips();
   }, []);
 
-  if (loading) {
-    return <div>Ładowanie tripów...</div>;
-  }
+  {loading ? (
+    <div className="flex justify-center items-center w-full">
+      <div className="flex items-center gap-3 py-4 mx-auto">
+        <Skeleton className="w-12 h-12 rounded-full" />
+        <Skeleton className="h-8 w-48" />
+      </div>
+    </div>
+  ) : trips.length > 0 && (
+    <div className="flex justify-center items-center w-full">
+      <div className="flex items-center gap-3 py-4 mx-auto">
+        <Image src="/images/icons/loop.png" alt="Loop" width={50} height={50} />
+        <p className="text-primary text-2xl font-semibold">Specjalnie dla Ciebie</p>
+      </div>
+    </div>
+  )}
 
   return <TripPropositionsClient initialTrips={trips} />;
 }
