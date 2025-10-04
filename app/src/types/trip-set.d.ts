e341e2816c2899e54771747d24f53113965fe9ef
Konrad Kałużny <konrad.kaluzny@ceneo.pl>
2025-10-04 23:02:00 +0200

    Add trip-related interfaces for location, transport, and accommodation in trip-set.d.ts

--- app/src/types/trip-set.d.ts
+++ app/src/types/trip-set.d.ts
@@ -1,7 +1,34 @@
+export interface LocationInTrip {
+  country: string;
+  region: string;
+  city: string;
+}
+
+export interface TransportInTrip {
+  from: LocationInTrip;
+  fromDate: string;
+  destination: LocationInTrip;
+  destinationDate: string;
+  price: number;
+  name: string;
+}
+
+export interface AccommodationInTrip {
+  location: LocationInTrip;
+  date: string;
+  price: number;
+  beds: number;
+  name: string;
+  description: string;
+  images: string[];
+}
+
 export interface Destination {
   order: number;
-  transportId: string;
-  accommodationId: [string] | null;
+  transport?: TransportInTrip;
+  accommodation?: AccommodationInTrip | null;
+  transportId?: string;
+  accommodationId?: [string] | null;
 }
 
 export interface TripSet {
